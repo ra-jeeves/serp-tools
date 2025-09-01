38baf9cfe1da12c6110694812ee5f208cf8469d7
Rajeev R Sharma <i.rarsh@gmail.com>
2025-09-01 10:26:42 +0530

    chore: retry without assetPrefix

--- apps/files/next.config.ts
+++ apps/files/next.config.ts
@@ -1,17 +1,29 @@
-import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
+import type { NextConfig } from "next";
 
-export default (phase: string) => {
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const nextConfig = {
-    output: 'export',
-    basePath: '/files',
-    transpilePackages: ["@serp-tools/ui"],
-    trailingSlash: true,
-    assetPrefix: isDev ? undefined : 'https://ra-jeeves.github.io/serp-tools',
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: "/files",
+  transpilePackages: ["@serp-tools/ui"],
+  trailingSlash: true,
+};
 
-  return nextConfig
-}
+export default nextConfig;
+
+// import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
+
+// export default (phase: string) => {
+//   const isDev = phase === PHASE_DEVELOPMENT_SERVER
+//   /**
+//    * @type {import('next').NextConfig}
+//    */
+//   const nextConfig = {
+//     output: 'export',
+//     basePath: '/files',
+//     transpilePackages: ["@serp-tools/ui"],
+//     trailingSlash: true,
+//     assetPrefix: isDev ? undefined : 'https://ra-jeeves.github.io/serp-tools',
+//   }
+
+//   return nextConfig
+// }
--- apps/tools/next.config.ts
+++ apps/tools/next.config.ts
@@ -1,17 +1,27 @@
-import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/tools',
+  transpilePackages: ["@serp-tools/ui"],
+  trailingSlash: true,
+};
 
-export default (phase: string) => {
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const nextConfig = {
-    output: 'export',
-    basePath: '/tools',
-    transpilePackages: ["@serp-tools/ui"],
-    trailingSlash: true,
-    assetPrefix: isDev ? undefined : 'https://ra-jeeves.github.io/serp-tools',
-  }
+export default nextConfig;
 
-  return nextConfig
-}
+// import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
+
+// export default (phase: string) => {
+//   const isDev = phase === PHASE_DEVELOPMENT_SERVER
+//   /**
+//    * @type {import('next').NextConfig}
+//    */
+//   const nextConfig = {
+//     output: 'export',
+//     basePath: '/tools',
+//     transpilePackages: ["@serp-tools/ui"],
+//     trailingSlash: true,
+//     assetPrefix: isDev ? undefined : 'https://ra-jeeves.github.io/serp-tools',
+//   }
+
+//   return nextConfig
+// }
